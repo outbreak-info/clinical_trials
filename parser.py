8053aa8968a5441e57dca4de5d3eda062e103f04
Laura Hughes <flaneuse@users.noreply.github.com>
2020-04-23 13:11:11 -0700

    converting to clinicaltrials.gov API (gunk)

--- parser.py
+++ parser.py
@@ -1,9 +1,43 @@
 import pandas as pd
 import xlrd
 import urllib
+import requests
 
 DATA_URL = "https://bit.ly/3axnfup"
-COL_NAMES = ["@type", "_id", "identifier", "identifierSource", "versionDate", "name", "alternateName", "funder"]
+CT_API = 'https://clinicaltrials.gov/api/query/full_studies?expr=(%22covid-19%22%20OR%20%22sars-cov-2%22)&min_rnk=1&max_rnk=1&fmt=json'
+CT_API = 'https://clinicaltrials.gov/api/query/study_fields?expr=(%22covid-19%22%20OR%20%22sars-cov-2%22)&min_rnk=1&max_rnk=1&fmt=json&fields=Acronym,NCTId'
+COL_NAMES = ["@type", "_id", "identifier", "identifierSource", "versionDate", "name", "alternateName"]
+
+resp = requests.get(CT_API)
+j = resp.json()
+j
+studies = j["FullStudiesResponse"]["FullStudies"]
+studies[0].keys()
+x =[study["Study"] for study in studies]
+pd.DataFrame([study["Study"] for study in studies])
+
+pd.DataFrame([study[item] for study in x for item in study])
+
+def flattenJson(arr):
+    flat_list = []
+
+    for study in arr:
+        obj = {}
+        for key in study:
+            print(key)
+            for innerKey in study[key]:
+                obj[innerKey] = study[key][innerKey]
+        flat_list.append(obj)
+    return(flat_list)
+
+z = flattenJson(x)
+y = flattenJson(z)
+
+
+pd.DataFrame(y).columns
+
+pd.DataFrame(flat_list)
+pd.DataFrame([item["ProtocolSection"] for item in x])
 
 def formatExcelDate(x, format="%Y-%m-%d"):
     dt = pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S').strftime(format)
@@ -17,7 +51,7 @@ def listify(row, col_names):
     return(arr)
 
 
-def getUminTrials(url, col_names):
+def getUSTrials(url, col_names):
     # Import the UMIN COVID-19 Clinical Trials registry list as .xls; required encoding is latin-1
     file_name, headers = urllib.request.urlretrieve(DATA_URL)
     data = xlrd.open_workbook(file_name, encoding_override='latin_1')
@@ -33,13 +67,15 @@ def getUminTrials(url, col_names):
     df["versionDate"] = df["Last Refreshed on"].apply(lambda x: formatExcelDate(x))
     df["name"] = df["Scientific title"]
     df["alternateName"] = df.apply(lambda x: listify(x, ["Acronym", "Public title"]), axis=1)
+    df["funder"] = df.apply(lambda x: organizationify()
 
     print(df[col_names].head(3).to_json(orient="records"))
 
     return(df)
 
 # def convertExcelDate(date):
-x = getUminTrials(DATA_URL, COL_NAMES)
+x = getUSTrials(DATA_URL, COL_NAMES)
 
 print(x.loc[0, COL_NAMES])
 x.loc[0, "alternateName"]
+x.Intervention.value_counts()
