70a23dade4feed7dcb2317efb3c69aef92c06809
Laura Hughes <flaneuse@users.noreply.github.com>
2020-05-05 13:25:13 -0700

    loop over all IDs to get NCT IDs > 1000

--- parser.py
+++ parser.py
@@ -421,7 +421,8 @@ def getArms(row):
                             if (f"{item['InterventionType']}: {item['InterventionName']}" == intervention_name):
                                 iObj["category"] = item["InterventionType"].lower()
                                 iObj["name"] = item["InterventionName"]
-                                iObj["description"] = item["InterventionDescription"]
+                                if("InterventionDescription" in item.keys()):
+                                    iObj["description"] = item["InterventionDescription"]
                         iArr.append(iObj)
                     obj["intervention"] = iArr
 
@@ -444,57 +445,83 @@ def getInterventions(row):
     return(arr)
 
 """
-Main function to execute the API calls, since they're limited to 100 full records at a time
+Helper function to get all the COVID-19-related NCT IDs.
+Since the ID fetch is limited to 1000 at a time, continuing to call till get no results found.
+Also, the NCT API doesn't seem to have explicit pagination, which means that the results returned are stoichastic.
+To try to mitigate this behavior, doing calls along a window to try to grab all the IDs.
 """
-
-
-def getUSTrials(query, col_names, json_output=True):
-    # Run one query to get the IDs of all the studies.
-    # Can't loop through numbers of studies à la pagination, since the API returns things in an inconsistent order
-    id_query = f"https://clinicaltrials.gov/api/query/study_fields?expr={query}&max_rnk=1000&fields=NCTId&fmt=json"
+def getIDSingle(query, minIdx):
+    id_query = f"https://clinicaltrials.gov/api/query/study_fields?expr={query}&min_rnk={int(minIdx*1000+1)}&max_rnk={int(minIdx*1000+1000)}&fields=NCTId&fmt=json"
     resp = requests.get(id_query)
     if resp.status_code == 200:
         raw_data = resp.json()
         num_results = raw_data["StudyFieldsResponse"]["NStudiesFound"]
-        ids = [item["NCTId"]
-               for item in raw_data["StudyFieldsResponse"]["StudyFields"]]
-        flat_ids = [item for sublist in ids for item in sublist]
-        results = pd.DataFrame()
-        i = 0
-        while i < ceil(num_results / 100):
-            print(f"Executing query {i+1} of {ceil(num_results / 100)}")
-            query_ids = " OR ".join(flat_ids[i * 100:(i + 1) * 100])
-            url = f"https://clinicaltrials.gov/api/query/full_studies?expr=({query_ids})&min_rnk=1&max_rnk=100&fmt=json"
-            df = getUSTrial(url, col_names)
-            results = pd.concat([results, df], ignore_index=True)
-            i += 1
-        # Double check that the numbers all agree
-        filtered = results[results._id.isin(flat_ids)]
-
-        if(len(flat_ids) != num_results):
-            print(
-                f"\nWARNING: number of IDs queried don't equal the number of results. {num_results} expected, but {len(flat_ids)} ids queried.\n")
-        if(len(results) != len(filtered)):
-            extra = results[~results._id.isin(flat_ids)]
-            print(f"\nWARNING: ids removed because they weren't in the initial query to get the ID list. Presumably, this record contains a COVID id somewhere in one of its other fields but is not a COVID-19 clinical trial.")
-            print(extra._id)
-        if(sum(filtered.duplicated(subset="_id"))):
-            dupes = filtered[filtered.duplicated(subset="_id")]
-            print(
-                f"\nERROR: {sum(filtered.duplicated(subset='_id'))} duplicate IDs found:")
-            print(dupes._id)
-
-        if(json_output):
-            output = filtered[col_names].to_json(orient="records")
+        if(raw_data["StudyFieldsResponse"]["NStudiesReturned"] > 0):
+            ids = [item["NCTId"]
+                   for item in raw_data["StudyFieldsResponse"]["StudyFields"]]
+            flat_ids = [item for sublist in ids for item in sublist]
+            return({"ids": flat_ids, "total": num_results})
         else:
-            output = filtered[col_names]
-        return(output)
+            return(None)
+
+def getIDs(query):
+    i = 0
+    hasMoreResults = True
+    ids = []
+    while (hasMoreResults):
+        print(f"Getting IDs: call {int(i*2 + 1)}")
+        id_list = getIDSingle(query, i)
+        if(id_list is None):
+            hasMoreResults = False
+        else:
+            i+= 0.5
+            ids = ids + id_list["ids"]
+            num_results = id_list["total"]
+    unique_ids = pd.np.unique(ids)
+    return({"ids": unique_ids, "total": num_results})
+
+"""
+Main function to execute the API calls, since they're limited to 100 full records at a time
+"""
+def getUSTrials(query, col_names, json_output=True):
+    num_per_query = 100
+    # Run one query to get the IDs of all the studies.
+    # Can't loop through numbers of studies à la pagination, since the API returns things in an inconsistent order
+    id_dict = getIDs(query)
+    num_results = id_dict["total"]
+    ids = id_dict["ids"]
+    results = pd.DataFrame()
+    i = 0
+    while i < ceil(num_results / num_per_query):
+        print(f"Executing query {i+1} of {ceil(num_results / num_per_query)}")
+        query_ids = " OR ".join(ids[i * num_per_query:(i + 1) * num_per_query])
+        url = f"https://clinicaltrials.gov/api/query/full_studies?expr=({query_ids})&min_rnk=1&max_rnk=100&fmt=json"
+        df = getUSTrial(url, col_names)
+        results = pd.concat([results, df], ignore_index=True)
+        i += 1
+    # Double check that the numbers all agree
+    filtered = results[results._id.isin(ids)]
+
+    if(len(filtered) != num_results):
+        print(
+            f"\nWARNING: number of IDs queried don't equal the number of results. {num_results} expected, but {len(filtered)} records found.\n")
+    if(len(results) != len(filtered)):
+        extra = results[~results._id.isin(ids)]
+        print(f"\nWARNING: ids removed because they weren't in the initial query to get the ID list. Presumably, this record contains a COVID id somewhere in one of its other fields but is not a COVID-19 clinical trial.")
+        print(extra._id)
+    if(sum(filtered.duplicated(subset="_id"))):
+        dupes = filtered[filtered.duplicated(subset="_id")]
+        print(
+            f"\nERROR: {sum(filtered.duplicated(subset='_id'))} duplicate IDs found:")
+        print(dupes._id)
+
+    if(json_output):
+        output = filtered[col_names].to_json(orient="records")
+    else:
+        output = filtered[col_names]
+    return(output)
 
 
 # df = getUSTrial("https://clinicaltrials.gov/api/query/full_studies?expr=(NCT04356560%20OR%20NCT04330261%20OR%20NCT04361396%20OR%20NCT04345679%20OR%20NCT04360811%20OR%20NCT04333862%20OR%20NCT04347278%20OR%20NCT04347850%20OR%20NCT04303299%20OR%20NCT04342637%20OR%20NCT04339322%20OR%20NCT04323787%20OR%20NCT04323800%20OR%20NCT04355897%20OR%20NCT04352764%20OR%20NCT04343781%20OR%20NCT04334876%20OR%20NCT04361422%20OR%20NCT04349202)&fmt=json&min_rnk=1&max_rnk=100", COL_NAMES)
 df = getUSTrials(CT_QUERY, COL_NAMES, False)
-
-df.sample(1).iloc[0]["armGroup"]
-
-df[df._id=="NCT04351581"][""]
-df.sample(5).to_json("/Users/laurahughes/GitHub/umin-clinical-trials/outputs/NCT_parsed_sample.json", orient="records")
+# df.sample(5).to_json("/Users/laurahughes/GitHub/umin-clinical-trials/outputs/NCT_parsed_sample.json", orient="records")
